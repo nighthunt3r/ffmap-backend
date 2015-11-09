7e4d786d9e6294c4424ed41d254c7a3610216693
nighthunt3r <richardbischof@t-online.de>
2015-11-09 16:46:02 +0100

    Added export to GeoJSON-Format
    GeoJSON is a interchange format for geospatial data. Specified here: http://geojson.org/geojson-spec.html 
    It will be a huge improvement, because through GeoJSON mutliple geoinformation-system can handle the data and use it for presentation and analysis.

--- lib/nodelist.py
+++ lib/nodelist.py
@@ -22,3 +22,28 @@ def export_nodelist(now, nodedb):
         nodelist.append(node_out)
 
     return {"version": "1.0.1", "nodes": nodelist, "updated_at": now.isoformat()}
+
+def export_nodelistGeoJson(now,nodedb):
+    nodelist = list {}
+    
+    for node_id, node in nodedb["nodes"].items():
+        node_out = dict {}
+        
+        node_out["type"] = "Feature"
+        node_out["geometry"] = dict{}
+        node_out["geometry"]["type"] = "Point"
+        node_out["geometry"]["coordinates"] = [node["nodeinfo"]["location"]["latitude"], 
+                                               node["nodeinfo"]["location"]["longitude"]]
+        node_out["properties"] = dict{}
+        node_out["properties"]["status"] = dict{}
+        node_out["properties"]["status"]["online"] = node["flags"]["online"]
+        
+        if "lastseen" in node:
+            node_out["properties"]["status"]["lastcontact"] = node["flags"]["online"]
+            
+        if "clients" in node["statistics"]:
+            node_out["properties"]["status"]["clients"] = node["statistics"]["clients"]
+
+        nodelist.append(node_out)
+         
+    return {"type:":"FeatureCollection", "features":nodeList}
